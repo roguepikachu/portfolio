bf535ff6e2e139bd90bb878c52ab209ea6732897
Ayush Kumar <ayushshyamkumar888@gmail.com>
2025-06-06 21:19:13 +0530

    chore: adds app tsx base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -25,7 +24,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/ayush-portfolio">
           <Layout>
             <Routes>
               <Route path="/" element={<Home />} />
